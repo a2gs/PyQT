92d3564ed06ed74f37f24d00498c8c5ad646ccae
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-08-28 00:45:33 -0300

    textbox sample

--- textbox.py
+++ textbox.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# https://pythonspot.com/pyqt5-textbox-example/
+
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
 from PyQt5.QtGui import QIcon
